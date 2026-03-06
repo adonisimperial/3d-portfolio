afefff6aeb4ac8d9711f2f1c14050d095ff1d0f3
Adonis Imperial <adonisimperial@yahoo.com>
2026-03-06 20:05:47 +0800

    initialCommit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import LogoSection from "./sections/LogoSection"
+import NavBar from "./components/NavBar"
+import Hero from "./sections/Hero"
+import ShowcaseSection from "./sections/ShowcaseSection"
+import FeatureCards from "./sections/FeatureCards"
+import ExperienceSection from "./sections/ExperienceSection"
+
+const App = () => {
+  return (
+    <>
+      <NavBar />
+      <Hero />
+      <ShowcaseSection />
+      <LogoSection />
+      <FeatureCards />
+      <ExperienceSection />
+    </>
+  )
+}
+
+export default App
